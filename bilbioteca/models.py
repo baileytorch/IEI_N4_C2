b9814145a750582ba1dd95bcb8e1baad0ffc5dc9
Erick Bailey <baileytorch@hotmail.com>
2026-09-11 15:11:18 -0300

    Aplicando migraciones

--- bilbioteca/models.py
+++ bilbioteca/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Pais(models.Model):
+    nombre = models.CharField(max_length=50,null=False)
+    nacionalidad = models.CharField(max_length=30,null=False)
+    iso_2 = models.CharField(max_length=2,null=False)
+    iso_3 = models.CharField(max_length=3,null=False)
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100,null=False)
+    pseudonimo = models.CharField(max_length=50,null=True)
+    nacionalidad = models.ForeignKey(Pais,on_delete=models.CASCADE)
+    fecha_nacimiento = models.DateField(null=True)
+    fecha_defuncion = models.DateField(null=True)
